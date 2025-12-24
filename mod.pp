043db542ebb4fe6399de9ecc9f40a41c253b624b
Chi Duong <chi.duong@stratuscyber.com>
2025-12-24 06:49:35 -0800

    first test

--- mod.pp
+++ mod.pp
@@ -1,9 +1,4 @@
 mod "fedramp_20x_dashboards" {
   title       = "FedRAMP 20x Dashboards"
   description = "Custom dashboards for Steampipe data visualization"
-  require {
-    mod "github.com/turbot/steampipe-mod-aws-compliance" {
-      version = ">=0.1.0"
-    }
-  }
 }
