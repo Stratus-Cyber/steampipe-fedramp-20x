2f598dfb9a81d22bb68ffdac7edce9cf912eb49c
Chi Duong <chi.duong@stratuscyber.com>
2026-01-19 12:25:43 -0800

    remove version key

--- mod.pp
+++ mod.pp
@@ -1,7 +1,6 @@
 mod "fedramp_20x" {
   title       = "FedRAMP 20x Compliance"
   description = "FedRAMP 20x Key Security Indicators (KSI) compliance benchmarks and controls for AWS."
-  version     = "1.0.0"
 
   require {
     plugin "aws" {
